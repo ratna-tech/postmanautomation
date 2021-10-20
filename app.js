dbcb9b3fbbba3ae73ad9146116b745618c1f1c42
ratna-tech <ratnasinha012@gmail.com>
2021-10-20 16:44:10 -0400

    app.js

--- app.js
+++ app.js
@@ -1,3 +1,13 @@
+// These are the Node.js modules used for this Express app.
+// They are installed using NPM
+var express = require('express');
+var cors = require('cors');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+
+// This creates the Express app which is configured below.
+var app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
@@ -9,5 +19,4 @@ app.use('/api', require('./api'));
 // This comes into play when we deploy the application to
 // Cloud Functions.
 module.exports = app;
-
 
